7d3e6d9bb3cdbf73cea3cecc273c7faee1d97a78
bex3 <Rebeccam.bearden@gmail>
2016-09-30 13:55:46 -0400

    starting unit testing

--- src/test/java/com/tiy/hackathon/DemoApplicationTests.java
+++ src/test/java/com/tiy/hackathon/DemoApplicationTests.java
@@ -2,15 +2,70 @@ package com.tiy.hackathon;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
+	@Autowired
+	EventRepository events;
+	@Autowired
+	UserRepository users;
+
+//    @Autowired
+//     ContactRepository contacts;
+
+
 	@Test
 	public void contextLoads() {
+
+	}
+
+	@Test
+	public void testCreateEvent() throws Exception {
+		User tester = new User();
+
+		tester.email = "Unittester@gmail.com";
+		tester.displayName = "OMG Unit Tester";
+		tester.password = "theBestTesterInTheWorld";
+
+		users.save(tester);
+
+		int userID = tester.getId();
+
+		
+		assertEquals();
+	}
+
+	@Test
+	public void testCreateUser() throws Exception {
+
+	}
+
+	@Test
+	public void testCheckInForUser() throws Exception {
+
+	}
+
+	@Test
+	public void testEditEvent() throws Exception {
+
+	}
+
+	@Test
+	public void testListForUsersAtEvent() throws Exception {
+
+	}
+
+	@Test
+	public void testGetMyFriends() throws Exception {
+
 	}
 
 }
