c1c56e3ff207f1fe099c213737e4c5bca063478a
Brett Gordon <gordonbrett89@gmail.com>
2016-09-30 10:57:09 -0400

    js function written, not working yet

--- src/main/java/com/tiy/hackathon/EventController.java
+++ src/main/java/com/tiy/hackathon/EventController.java
@@ -25,27 +25,27 @@ public class EventController {
 		return "home";
 	}
 
-	@RequestMapping(path = "/newUser", method = RequestMethod.POST)
-	public String newUser(HttpSession session, String email, String displayName, String password) {
-	User user = users.findFirstByEmail(email);
-		if (user == null) {
-			user = new User(email, displayName, password);
-			users.save(user);
-		}
-		return "redirect:/";
-	}
-
-	@RequestMapping(path = "/login", method = RequestMethod.POST)
-	public String login(HttpSession session, String email, String password) throws Exception {
-		User user = users.findFirstByEmail(email);
-		if (user == null) {
-			throw new Exception("User does not exist!");
-		} else if (!password.equals(user.getPassword())) {
-			throw new Exception("Password is incorrect");
-		}
-		session.setAttribute("user", user);
-		return "redirect:/events";
-	}
+//	@RequestMapping(path = "/newUser", method = RequestMethod.POST)
+//	public String newUser(HttpSession session, String email, String displayName, String password) {
+//	User user = users.findFirstByEmail(email);
+//		if (user == null) {
+//			user = new User(email, displayName, password);
+//			users.save(user);
+//		}
+//		return "redirect:/";
+//	}
+//
+//	@RequestMapping(path = "/login", method = RequestMethod.POST)
+//	public String login(HttpSession session, String email, String password) throws Exception {
+//		User user = users.findFirstByEmail(email);
+//		if (user == null) {
+//			throw new Exception("User does not exist!");
+//		} else if (!password.equals(user.getPassword())) {
+//			throw new Exception("Password is incorrect");
+//		}
+//		session.setAttribute("user", user);
+//		return "redirect:/events";
+//	}
 
 	@RequestMapping(path = "/events", method = RequestMethod.GET)
 	public String events() {
